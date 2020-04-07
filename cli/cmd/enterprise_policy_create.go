a7cc5d7d2037114550e0d2b52fd37384ba02a10a
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-04-07 20:18:08 +0000

    update enterprise policy

--- cli/cmd/enterprise_policy_create.go
+++ cli/cmd/enterprise_policy_create.go
@@ -16,13 +16,13 @@ func (r *runners) InitEnterprisePolicyCreate(parent *cobra.Command) {
 		Long: `Create a new policy that can later be assigned to a channel.
 
   Example:
-  replicated enteprise channel policy --name MyPolicy --description 'A sample policy'`,
+  replicated enteprise policy create --policy-file myfile.rego --name MyPolicy --description 'A sample policy'`,
 	}
 	parent.AddCommand(cmd)
 
 	cmd.Flags().StringVar(&r.args.enterprisePolicyCreateName, "name", "", "The name of this policy")
 	cmd.Flags().StringVar(&r.args.enterprisePolicyCreateDescription, "description", "", "A longer description of this policy")
-	cmd.Flags().StringVar(&r.args.enterprisePolicyCreateFile, "policy-file", "", "The filename containing an OPA policy file")
+	cmd.Flags().StringVar(&r.args.enterprisePolicyCreateFile, "policy-file", "", "The filename containing an OPA policy")
 
 	cmd.RunE = r.enterprisePolicyCreate
 }
