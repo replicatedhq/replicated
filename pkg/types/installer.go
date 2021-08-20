8abe28a8c65f9fd1266899c04fd664919c67456d
jeffreygolden <jeff@replicated.com>
2021-08-20 17:10:39 +0000

    removing unnecessary types

--- pkg/types/installer.go
+++ pkg/types/installer.go
@@ -1,8 +1,6 @@
 package types
 
 import (
-	"time"
-
 	"github.com/replicatedhq/replicated/pkg/util"
 )
 
@@ -28,28 +26,4 @@ type PromoteInstallerRequest struct {
 	Sequence     int64  `json:"sequence"`
 	VersionLabel string `json:"versionLabel"`
 	ChannelID    string `json:"channelId"`
-}
-
-type EnterpriseChannel struct {
-	Description    string               `json:"description,omitempty"`
-	EnterpriseName string               `json:"enterprise_name,omitempty"`
-	Id             string               `json:"id,omitempty"`
-	Installer      *EnterpriseInstaller `json:"installer,omitempty"`
-	Name           string               `json:"name,omitempty"`
-	Policies       []EnterprisePolicy   `json:"policies,omitempty"`
-}
-
-type EnterpriseInstaller struct {
-	Id        string `json:"id,omitempty"`
-	PartnerId string `json:"partner_id,omitempty"`
-	Yaml      string `json:"yaml,omitempty"`
-}
-
-type EnterprisePolicy struct {
-	CreatedAt   time.Time `json:"created_at,omitempty"`
-	Description string    `json:"description,omitempty"`
-	Id          string    `json:"id,omitempty"`
-	Name        string    `json:"name,omitempty"`
-	PartnerId   string    `json:"partner_id,omitempty"`
-	Policy      string    `json:"policy,omitempty"`
 }
