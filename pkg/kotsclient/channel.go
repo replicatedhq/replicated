2df8a0564f573e23881adff805603d84e809ee2c
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2021-08-24 16:41:05 +0000

    fix promoting kots releases

--- pkg/kotsclient/channel.go
+++ pkg/kotsclient/channel.go
@@ -125,7 +125,11 @@ func (c *VendorV3Client) CreateChannel(appID, name, description string) (*types.
 }
 
 func (c *VendorV3Client) GetChannel(appID string, channelID string) (*channels.AppChannel, []channels.ChannelRelease, error) {
-	response := types.KotsChannel{}
+	type getChannelResponse struct {
+		Channel types.KotsChannel `json:"channel"`
+	}
+
+	response := getChannelResponse{}
 	url := fmt.Sprintf("/v3/app/%s/channel/%s", appID, url.QueryEscape(channelID))
 	err := c.DoJSON("GET", url, http.StatusOK, nil, &response)
 	if err != nil {
@@ -133,11 +137,11 @@ func (c *VendorV3Client) GetChannel(appID string, channelID string) (*channels.A
 	}
 
 	channelDetail := channels.AppChannel{
-		Id:              response.Id,
-		Name:            response.Name,
-		Description:     response.Description,
-		ReleaseLabel:    response.CurrentVersion,
-		ReleaseSequence: int64(response.ReleaseSequence),
+		Id:              response.Channel.Id,
+		Name:            response.Channel.Name,
+		Description:     response.Channel.Description,
+		ReleaseLabel:    response.Channel.CurrentVersion,
+		ReleaseSequence: int64(response.Channel.ReleaseSequence),
 	}
 	return &channelDetail, nil, nil
 }
