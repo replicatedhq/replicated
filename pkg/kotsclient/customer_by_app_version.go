d3d6a9e7b956de4305172348216c4cef31bbcad2
Andrew Storms <storms@replicated.com>
2023-05-16 09:30:42 -0700

    use CustomerListWithInstancesResponse type. fix the slice append

--- pkg/kotsclient/customer_by_app_version.go
+++ pkg/kotsclient/customer_by_app_version.go
@@ -8,20 +8,10 @@ import (
 	"github.com/replicatedhq/replicated/pkg/types"
 )
 
-// var _ error = ErrAppVersionNotFound{}
-//
-// type ErrAppVersionNotFound struct {
-//Name string
-//}
-
-// func (e ErrAppVersionNotFound) Error() string {
-//return fmt.Sprintf("version %q not found", e.Name)
-//}
-
-//type CustomerListResponse struct {
-//Customers      []types.Customer `json:"customers"`
-//TotalCustomers int              `json:"totalCustomers"`
-//}
+type CustomerListWithInstancesResponse struct {
+	Customers      []types.Customer `json:"customers"`
+	TotalCustomers int              `json:"totalCustomers"`
+}
 
 func (c *VendorV3Client) ListCustomersByAppVersion(appID string, appVersion string, appType string) ([]types.Customer, error) {
 	println("--------------DEBUG-------------------------")
@@ -34,38 +24,53 @@ func (c *VendorV3Client) ListCustomersByAppVersion(appID string, appVersion stri
 	matchingCustomers := []types.Customer{}
 	page := 0
 	for {
-		resp := CustomerListResponse{}
+		resp := CustomerListWithInstancesResponse{}
 		path := fmt.Sprintf("/v3/app/%s/customers?currentPage=%d", appID, page)
 		err := c.DoJSON("GET", path, http.StatusOK, nil, &resp)
 		if err != nil {
-			return nil, errors.Wrapf(err, "list customers page %d", page)
+			return nil, errors.Wrapf(err, "List Customers By App Version page %d", page)
 		}
 
-		for index, customer := range resp.Customers {
-			fmt.Printf("DEBUG: customer.Name: %v\n", customer.Name)
-			// for each customer, print the instances
+		for customersIndex, customer := range resp.Customers {
+			fmt.Println("-----------------------------")
+			fmt.Printf("DEBUG: customer.Name:    %v\n", customer.Name)
+			fmt.Printf("DEBUG: customer.ID:      %v\n", customer.ID)
+			fmt.Printf("DEBUG: customersIndex:   %v\n", customersIndex)
 			for _, instance := range customer.Instances {
-				fmt.Printf("         instance.InstanceId: %v\n", instance.InstanceId)
-				// for each instance print the VersionHistory VersionLabel
-				for _, versionHistory := range instance.VersionHistory {
-					if versionHistory.VersionLabel == appVersion {
-						fmt.Printf("           versionHistory.VersionLabel: %v MATCH\n", versionHistory.VersionLabel)
-						matchingCustomers = append(matchingCustomers, resp.Customers[index])
+				fmt.Printf("      DEBUG: instance.InstanceId: %v\n", instance.InstanceId)
+				for versionIndex, versionHistory := range instance.VersionHistory {
+					fmt.Println("      DEBUG: versionHistory: ", versionHistory)
+					if versionHistory.VersionLabel == appVersion && versionIndex == 0 {
+						fmt.Printf("      DEBUG: versionHistory.VersionLabel: %v MATCH\n", versionHistory.VersionLabel)
+						// Append the customer to the matchingCustomers slice
+						// matchingCustomers = append(matchingCustomers, customer)
+						// Apppend the customer and
+						// the instance to the matchingCustomers slice
+						// matchingCustomers = append(matchingCustomers, customer, instance)
+						// Append the customer and
+						// the instance and
+						// the versionHistory to the matchingCustomers slice
+						tempCustomer := customer
+						tempCustomer.Instances = []types.Instance{instance}
+						tempCustomer.Instances[0].VersionHistory = []types.VersionHistory{versionHistory}
+						matchingCustomers = append(matchingCustomers, tempCustomer)
+						//matchingCustomers = append(matchingCustomers, customer, instance, versionHistory)
+
 					} else {
-						fmt.Printf("           versionHistory.VersionLabel: %v\n", versionHistory.VersionLabel)
+						fmt.Printf("      DEBUG: versionHistory.VersionLabel: %v\n", versionHistory.VersionLabel)
 					}
 				}
 			}
-
 		}
-
 		if len(matchingCustomers) == resp.TotalCustomers || len(resp.Customers) == 0 {
 			break
 		}
-
 		page = page + 1
 	}
 
 	println("--------------DEBUG-------------------------\n")
+	if len(matchingCustomers) == 0 {
+		return matchingCustomers, fmt.Errorf("no matching customers found for app %q and version %q ", appID, appVersion)
+	}
 	return matchingCustomers, nil
 }
