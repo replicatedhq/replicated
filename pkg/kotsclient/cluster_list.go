a976b249d0e5dce428d169dae427059d06d7a346
Tim Ousborne <timo@replicated.com>
2023-05-25 09:51:48 -0600

    change cluster_ls paramters back to be queries

--- pkg/kotsclient/cluster_list.go
+++ pkg/kotsclient/cluster_list.go
@@ -1,40 +1,30 @@
 package kotsclient
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/replicatedhq/replicated/pkg/types"
 )
 
-type ListClustersRequest struct {
-	IncludeTerminated bool     `json:"include_terminated"`
-	ClusterIDs        []string `json:"cluster_ids"`
-	StartTime         int64    `json:"start_time"`
-	EndTime           int64    `json:"end_time"`
-}
+type ListClusterRequest struct{}
 
 type ListClustersResponse struct {
-	Continue bool             `json:"continue"`
 	Clusters []*types.Cluster `json:"clusters"`
 }
 
 type ListClustersOpts struct {
 	IncludeTerminated bool
-	ClusterIDs        []string
 	StartTime         time.Time
 	EndTime           time.Time
 }
 
 func (c *VendorV3Client) ListClusters(opts ListClustersOpts) ([]*types.Cluster, error) {
-	reqBody := &ListClustersRequest{
-		IncludeTerminated: opts.IncludeTerminated,
-		ClusterIDs:        opts.ClusterIDs,
-		StartTime:         opts.StartTime.Unix(),
-		EndTime:           opts.EndTime.Unix(),
-	}
+	reqBody := ListClusterRequest{}
 	clusters := ListClustersResponse{}
-	err := c.DoJSON("GET", "/v3/clusters", http.StatusOK, reqBody, &clusters)
+	err := c.DoJSON("GET", fmt.Sprintf("/v3/clusters?include-terminated=%s&start-time=%s&end-time=%s", strconv.FormatBool(opts.IncludeTerminated), strconv.FormatInt(opts.StartTime.Unix(), 10), strconv.FormatInt(opts.EndTime.Unix(), 10)), http.StatusOK, reqBody, &clusters)
 	if err != nil {
 		return nil, err
 	}
