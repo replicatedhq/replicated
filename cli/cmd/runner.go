d6cd7ea88bc665ee9179aca213886f685c714903
Dmitriy Ivolgin <dmitriy@replicated.com>
2023-06-30 14:23:41 -0700

    Allow linting Builders apps (#277)

--- cli/cmd/runner.go
+++ cli/cmd/runner.go
@@ -65,6 +65,7 @@ type runnerArgs struct {
 	// Add Create Release Lint
 	createReleaseLint     bool
 	lintReleaseYamlDir    string
+	lintReleaseChart      string
 	lintReleaseFailOn     string
 	releaseOptional       bool
 	releaseRequired       bool
