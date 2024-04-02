b15c129a66d03e012164ca3a4309ee6bb1eabb75
Brian Rhea <brianrhea@gmail.com>
2024-04-02 16:43:53 -0600

    Remove reference to KOTS in app_create and app_ls command descriptions

--- cli/cmd/app_create.go
+++ cli/cmd/app_create.go
@@ -11,8 +11,8 @@ import (
 func (r *runners) InitAppCreate(parent *cobra.Command) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:          "create NAME",
-		Short:        "create kots apps",
-		Long:         `create kots apps`,
+		Short:        "create apps",
+		Long:         `create apps`,
 		RunE:         r.createApp,
 		SilenceUsage: true,
 	}
