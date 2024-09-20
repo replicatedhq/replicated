83af1fa2822ca3fc48a78ef1861424a2d489a8bf
divolgin <dmitriy@replicated.com>
2024-09-20 15:22:21 -0700

    Deprecate --chart flag

--- cli/cmd/release_create.go
+++ cli/cmd/release_create.go
@@ -59,6 +59,9 @@ func (r *runners) InitReleaseCreate(parent *cobra.Command) error {
 	cmd.Flags().MarkHidden("yaml-file")
 	cmd.Flags().MarkHidden("yaml")
 
+	// deprecated
+	cmd.Flags().MarkHidden("chart")
+
 	cmd.RunE = r.releaseCreate
 	return nil
 }
@@ -242,6 +245,8 @@ Prepared to create release with defaults:
 	}
 
 	if r.args.createReleaseChart != "" {
+		printChartDeprecationWarning()
+
 		fmt.Fprint(r.w, "You are creating a release that will only be installable with the helm CLI.\n"+
 			"For more information, see \n"+
 			"https://docs.replicated.com/vendor/helm-install#about-helm-installations-with-replicated\n")
