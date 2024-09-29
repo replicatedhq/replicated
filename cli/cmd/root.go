64cb875164f197bc846e3d1d78867b43208b2278
Josh De Winne <joris.dewinne@gmail.com>
2024-09-28 17:41:15 -0700

    adding nodegroup ls for vm

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -262,6 +262,9 @@ func Execute(rootCmd *cobra.Command, stdin io.Reader, stdout io.Writer, stderr i
 	runCmds.InitVMVersions(vmCmd)
 	runCmds.InitVMRemove(vmCmd)
 
+	vmNodeGroupCmd := runCmds.InitVMNodeGroup(vmCmd)
+	runCmds.InitVMNodeGroupList(vmNodeGroupCmd)
+
 	vmUpdateCmd := runCmds.InitVMUpdateCommand(vmCmd)
 	runCmds.InitVMUpdateTTL(vmUpdateCmd)
 
