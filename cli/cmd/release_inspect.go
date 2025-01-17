1331fe78cf9f04a59c78a644f316d8ca607abef4
divolgin <dmitriy@replicated.com>
2025-01-17 12:28:11 -0800

    Unhide release inspect command

--- cli/cmd/release_inspect.go
+++ cli/cmd/release_inspect.go
@@ -12,12 +12,25 @@ import (
 
 func (r *runners) InitReleaseInspect(parent *cobra.Command) {
 	cmd := &cobra.Command{
-		Use:   "inspect SEQUENCE",
-		Short: "Print the YAML config for a release",
-		Long:  "Print the YAML config for a release",
+		Use:   "inspect RELEASE_SEQUENCE",
+		Short: "Long: information about a release",
+		Long: `Show information about the specified application release.
+
+This command displays detailed information about a specific release of an application.
+
+The output can be customized using the --output flag to display results in
+either table or JSON format.
+		`,
+		Example: `  # Display information about a release
+  replicated release inspect 123
+
+  # Display information about a release in JSON format
+  replicated release inspect 123 --output json`,
+		Args: cobra.ExactArgs(1),
 	}
-	cmd.Hidden = true // Not supported in KOTS
+	cmd.Flags().StringVar(&r.outputFormat, "output", "table", "The output format to use. One of: json|table (default: table)")
 	parent.AddCommand(cmd)
+
 	cmd.RunE = r.releaseInspect
 }
 
@@ -26,9 +39,6 @@ func (r *runners) releaseInspect(_ *cobra.Command, args []string) error {
 		return errors.New("no app specified")
 	}
 
-	if len(args) != 1 {
-		return errors.New("release sequence is required")
-	}
 	seq, err := strconv.ParseInt(args[0], 10, 64)
 	if err != nil {
 		return fmt.Errorf("Failed to parse sequence argument %s", args[0])
@@ -42,5 +52,5 @@ func (r *runners) releaseInspect(_ *cobra.Command, args []string) error {
 		return err
 	}
 
-	return print.Release(r.w, release)
+	return print.Release(r.outputFormat, r.w, release)
 }
