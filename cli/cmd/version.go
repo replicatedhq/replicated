bf840e119965a4d362759a2f8a5e6e981547802e
Marc Campbell <marc.e.campbell@gmail.com>
2025-03-08 10:34:59 -0700

    make update checks quicker (#513)

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -17,10 +17,40 @@ func Version() *cobra.Command {
 		Short: "Print the current version and exit",
 		Long:  `Print the current version and exit`,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			currentVersion := version.Version()
+				
+			// For version command, do a synchronous update check
+			updateChecker, err := version.NewUpdateChecker(currentVersion, "replicatedhq/replicated/cli") 
+			if err == nil {
+				// If we're in a development build or unknown version, still try to get latest
+				// version info but don't compare versions
+				updateInfo, err := updateChecker.GetUpdateInfo()
+				if err == nil && updateInfo != nil {
+					// Update the build info with latest version
+					version.SetUpdateInfo(updateInfo)
+					// Also save to cache for future commands
+					version.SaveUpdateCache(currentVersion, updateInfo)
+				}
+			}
+			
+			// Now get the (potentially updated) build info
 			build := version.GetBuild()
 
 			if !versionJson {
-				version.Print()
+				// Special handling for development/unknown version when printing
+				if currentVersion == "unknown" || currentVersion == "development" {
+					fmt.Printf("replicated version %s (development build)\n", currentVersion)
+					if build.UpdateInfo != nil {
+						fmt.Printf("Latest release: %s\n", build.UpdateInfo.LatestVersion)
+						if build.UpdateInfo.CanUpgradeInPlace {
+							fmt.Printf("To automatically upgrade, run \"replicated version upgrade\"\n")
+						} else if build.UpdateInfo.ExternalUpgradeCommand != "" {
+							fmt.Printf("To upgrade, run \"%s\"\n", build.UpdateInfo.ExternalUpgradeCommand)
+						}
+					}
+				} else {
+					version.Print()
+				}
 			} else {
 				versionInfo, err := json.MarshalIndent(build, "", "    ")
 				if err != nil {
