2d117914c143eb15787ea39a6d5cb3d614333896
Marc Campbell <marc.e.campbell@gmail.com>
2025-04-15 14:55:42 -0700

    Optionally send optional values

--- pkg/kotsclient/customer_create.go
+++ pkg/kotsclient/customer_create.go
@@ -21,31 +21,31 @@ type CustomerChannel struct {
 }
 
 type CreateCustomerRequest struct {
-	Name                               string             `json:"name"`
-	Channels                           []CustomerChannel  `json:"channels"`
-	CustomID                           string             `json:"custom_id"`
-	AppID                              string             `json:"app_id"`
-	Type                               string             `json:"type"`
-	ExpiresAt                          string             `json:"expires_at"`
-	IsAirgapEnabled                    bool               `json:"is_airgap_enabled"`
-	IsGitopsSupported                  bool               `json:"is_gitops_supported"`
-	IsSnapshotSupported                bool               `json:"is_snapshot_supported"`
-	IsKotsInstallEnabled               bool               `json:"is_kots_install_enabled"`
-	IsEmbeddedClusterDownloadEnabled   bool               `json:"is_embedded_cluster_download_enabled"`
-	IsEmbeddedClusterMultinodeEnabled  bool               `json:"is_embedded_cluster_multinode_enabled"`
-	IsGeoaxisSupported                 bool               `json:"is_geoaxis_supported"`
-	IsHelmVMDownloadEnabled            bool               `json:"is_helm_vm_download_enabled"`
-	IsIdentityServiceSupported         bool               `json:"is_identity_service_supported"`
-	IsInstallerSupportEnabled          bool               `json:"is_installer_support_enabled"`
-	IsSupportBundleUploadEnabled       bool               `json:"is_support_bundle_upload_enabled"`
-	IsDeveloperModeEnabled             bool               `json:"is_dev_mode_enabled"`
-	Email                              string             `json:"email,omitempty"`
-	EntitlementValues                  []EntitlementValue `json:"entitlementValues"`
+	Name                         string             `json:"name"`
+	Channels                     []CustomerChannel  `json:"channels"`
+	CustomID                     string             `json:"custom_id"`
+	AppID                        string             `json:"app_id"`
+	Type                         string             `json:"type"`
+	ExpiresAt                    string             `json:"expires_at"`
+	IsAirgapEnabled              bool               `json:"is_airgap_enabled"`
+	IsGitopsSupported            bool               `json:"is_gitops_supported"`
+	IsSnapshotSupported          bool               `json:"is_snapshot_supported"`
+	IsKotsInstallEnabled         bool               `json:"is_kots_install_enabled"`
+	IsGeoaxisSupported           bool               `json:"is_geoaxis_supported"`
+	IsHelmVMDownloadEnabled      bool               `json:"is_helm_vm_download_enabled"`
+	IsIdentityServiceSupported   bool               `json:"is_identity_service_supported"`
+	IsInstallerSupportEnabled    bool               `json:"is_installer_support_enabled"`
+	IsSupportBundleUploadEnabled bool               `json:"is_support_bundle_upload_enabled"`
+	IsDeveloperModeEnabled       bool               `json:"is_dev_mode_enabled"`
+	Email                        string             `json:"email,omitempty"`
+	EntitlementValues            []EntitlementValue `json:"entitlementValues"`
 
 	// These fields were added after the "built in" fields feature was released.
 	// If they are not pointer types, they will override the defaults.
-	IsHelmInstallEnabled *bool `json:"is_helm_install_enabled,omitempty"`
-	IsKurlInstallEnabled *bool `json:"is_kurl_install_enabled,omitempty"`
+	IsHelmInstallEnabled              *bool `json:"is_helm_install_enabled,omitempty"`
+	IsKurlInstallEnabled              *bool `json:"is_kurl_install_enabled,omitempty"`
+	IsEmbeddedClusterDownloadEnabled  *bool `json:"is_embedded_cluster_download_enabled,omitempty"`
+	IsEmbeddedClusterMultinodeEnabled *bool `json:"is_embedded_cluster_multinode_enabled,omitempty"`
 }
 
 type CreateCustomerResponse struct {
@@ -53,54 +53,54 @@ type CreateCustomerResponse struct {
 }
 
 type CreateCustomerOpts struct {
-	Name                               string
-	CustomID                           string
-	Channels                           []CustomerChannel
-	AppID                              string
-	ExpiresAt                          string
-	ExpiresAtDuration                  time.Duration
-	IsAirgapEnabled                    bool
-	IsGitopsSupported                  bool
-	IsSnapshotSupported                bool
-	IsKotsInstallEnabled               bool
-	IsHelmInstallEnabled               *bool
-	IsKurlInstallEnabled               *bool
-	IsEmbeddedClusterDownloadEnabled   bool
-	IsEmbeddedClusterMultinodeEnabled  bool
-	IsGeoaxisSupported                 bool
-	IsHelmVMDownloadEnabled            bool
-	IsIdentityServiceSupported         bool
-	IsInstallerSupportEnabled          bool
-	IsSupportBundleUploadEnabled       bool
-	IsDeveloperModeEnabled             bool
-	LicenseType                        string
-	Email                              string
-	EntitlementValues                  []EntitlementValue
+	Name                              string
+	CustomID                          string
+	Channels                          []CustomerChannel
+	AppID                             string
+	ExpiresAt                         string
+	ExpiresAtDuration                 time.Duration
+	IsAirgapEnabled                   bool
+	IsGitopsSupported                 bool
+	IsSnapshotSupported               bool
+	IsKotsInstallEnabled              bool
+	IsHelmInstallEnabled              *bool
+	IsKurlInstallEnabled              *bool
+	IsEmbeddedClusterDownloadEnabled  *bool
+	IsEmbeddedClusterMultinodeEnabled *bool
+	IsGeoaxisSupported                bool
+	IsHelmVMDownloadEnabled           bool
+	IsIdentityServiceSupported        bool
+	IsInstallerSupportEnabled         bool
+	IsSupportBundleUploadEnabled      bool
+	IsDeveloperModeEnabled            bool
+	LicenseType                       string
+	Email                             string
+	EntitlementValues                 []EntitlementValue
 }
 
 func (c *VendorV3Client) CreateCustomer(opts CreateCustomerOpts) (*types.Customer, error) {
 	request := &CreateCustomerRequest{
-		Name:                               opts.Name,
-		CustomID:                           opts.CustomID,
-		Channels:                           opts.Channels,
-		AppID:                              opts.AppID,
-		Type:                               opts.LicenseType,
-		IsAirgapEnabled:                    opts.IsAirgapEnabled,
-		IsGitopsSupported:                  opts.IsGitopsSupported,
-		IsSnapshotSupported:                opts.IsSnapshotSupported,
-		IsKotsInstallEnabled:               opts.IsKotsInstallEnabled,
-		IsHelmInstallEnabled:               opts.IsHelmInstallEnabled,
-		IsKurlInstallEnabled:               opts.IsKurlInstallEnabled,
-		IsEmbeddedClusterDownloadEnabled:   opts.IsEmbeddedClusterDownloadEnabled,
-		IsEmbeddedClusterMultinodeEnabled:  opts.IsEmbeddedClusterMultinodeEnabled,
-		IsGeoaxisSupported:                 opts.IsGeoaxisSupported,
-		IsHelmVMDownloadEnabled:            opts.IsHelmVMDownloadEnabled,
-		IsIdentityServiceSupported:         opts.IsIdentityServiceSupported,
-		IsInstallerSupportEnabled:          opts.IsInstallerSupportEnabled,
-		IsSupportBundleUploadEnabled:       opts.IsSupportBundleUploadEnabled,
-		IsDeveloperModeEnabled:             opts.IsDeveloperModeEnabled,
-		Email:                              opts.Email,
-		EntitlementValues:                  opts.EntitlementValues,
+		Name:                              opts.Name,
+		CustomID:                          opts.CustomID,
+		Channels:                          opts.Channels,
+		AppID:                             opts.AppID,
+		Type:                              opts.LicenseType,
+		IsAirgapEnabled:                   opts.IsAirgapEnabled,
+		IsGitopsSupported:                 opts.IsGitopsSupported,
+		IsSnapshotSupported:               opts.IsSnapshotSupported,
+		IsKotsInstallEnabled:              opts.IsKotsInstallEnabled,
+		IsHelmInstallEnabled:              opts.IsHelmInstallEnabled,
+		IsKurlInstallEnabled:              opts.IsKurlInstallEnabled,
+		IsEmbeddedClusterDownloadEnabled:  opts.IsEmbeddedClusterDownloadEnabled,
+		IsEmbeddedClusterMultinodeEnabled: opts.IsEmbeddedClusterMultinodeEnabled,
+		IsGeoaxisSupported:                opts.IsGeoaxisSupported,
+		IsHelmVMDownloadEnabled:           opts.IsHelmVMDownloadEnabled,
+		IsIdentityServiceSupported:        opts.IsIdentityServiceSupported,
+		IsInstallerSupportEnabled:         opts.IsInstallerSupportEnabled,
+		IsSupportBundleUploadEnabled:      opts.IsSupportBundleUploadEnabled,
+		IsDeveloperModeEnabled:            opts.IsDeveloperModeEnabled,
+		Email:                             opts.Email,
+		EntitlementValues:                 opts.EntitlementValues,
 	}
 
 	// if expiresAtDuration is set, calculate the expiresAt time
