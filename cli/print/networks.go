3862100d0cb7d61386cebb2278608842a48082fd
Kyle Squizzato <kyle@replicated.com>
2025-05-27 14:02:08 -0700

    chore(cmx): Remove network create/rm/join commands
    * Remove 'expires_at' column from network ls since expiration
      is directly affiliated with cluster/vm expiration.  The struct
      key for expires is still present for troubleshooting.
    * Update 'network' command help to explain how networks are created/removed.
    * Unhide 'network' commands.
    
    Signed-off-by: Kyle Squizzato <kyle@replicated.com>

--- cli/print/networks.go
+++ cli/print/networks.go
@@ -10,22 +10,32 @@ import (
 )
 
 // Table formatting
-var networksTmplTableHeaderSrc = `ID	NAME	STATUS	CREATED	EXPIRES	POLICY`
-var networksTmplTableRowSrc = `{{ range . -}}
-{{ .ID }}	{{ padding .Name 27	}}	{{ padding (printf "%s" .Status) 12 }}	{{ padding (printf "%s" (localeTime .CreatedAt)) 30 }}	{{if .ExpiresAt.IsZero}}{{ padding "-" 30 }}{{else}}{{ padding (printf "%s" (localeTime .ExpiresAt)) 30 }}{{end}}	{{if eq .Policy ""}}{{ padding "open" 30 }}{{else}}{{ padding (printf "%s" (.Policy)) 30 }}{{end}}
+var (
+	networksTmplTableHeaderSrc = `{{ padding "ID" 12 }}{{ padding "NAME" 30 }}{{ padding "STATUS" 12 }}{{ padding "CREATED" 25 }}POLICY`
+	networksTmplTableRowSrc    = `{{ range . -}}
+{{ padding .ID 12 }}{{ padding .Name 30 }}{{ padding (printf "%s" .Status) 12 }}{{ padding (printf "%s" (localeTime .CreatedAt)) 25 }}{{if eq .Policy ""}}{{ padding "open" 15 }}{{else}}{{ padding (printf "%s" (.Policy)) 15 }}{{end}}
 {{ end }}`
-var networksTmplTableSrc = fmt.Sprintln(networksTmplTableHeaderSrc) + networksTmplTableRowSrc
-var networksTmplTable = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplTableSrc))
-var networksTmplTableNoHeader = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplTableRowSrc))
+)
+
+var (
+	networksTmplTableSrc      = networksTmplTableHeaderSrc + "\n" + networksTmplTableRowSrc
+	networksTmplTable         = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplTableSrc))
+	networksTmplTableNoHeader = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplTableRowSrc))
+)
 
 // Wide table formatting
-var networksTmplWideHeaderSrc = `ID	NAME	STATUS	CREATED	EXPIRES	POLICY`
-var networksTmplWideRowSrc = `{{ range . -}}
-{{ .ID }}	{{ padding .Name 27	}}	{{ padding (printf "%s" .Status) 12 }}	{{ padding (printf "%s" (localeTime .CreatedAt)) 30 }}	{{if .ExpiresAt.IsZero}}{{ padding "-" 30 }}{{else}}{{ padding (printf "%s" (localeTime .ExpiresAt)) 30 }}{{end}}	{{if eq .Policy ""}}{{ padding "open" 30 }}{{else}}{{ padding (printf "%s" (.Policy)) 30 }}{{end}}
+var (
+	networksTmplWideHeaderSrc = `{{ padding "ID" 12 }}{{ padding "NAME" 30 }}{{ padding "STATUS" 12 }}{{ padding "CREATED" 25 }}POLICY`
+	networksTmplWideRowSrc    = `{{ range . -}}
+{{ padding .ID 12 }}{{ padding .Name 30 }}{{ padding (printf "%s" .Status) 12 }}{{ padding (printf "%s" (localeTime .CreatedAt)) 25 }}{{if eq .Policy ""}}{{ padding "open" 15 }}{{else}}{{ padding (printf "%s" (.Policy)) 15 }}{{end}}
 {{ end }}`
-var networksTmplWideSrc = fmt.Sprintln(networksTmplWideHeaderSrc) + networksTmplWideRowSrc
-var networksTmplWide = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplWideSrc))
-var networksTmplWideNoHeader = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplWideRowSrc))
+)
+
+var (
+	networksTmplWideSrc      = networksTmplWideHeaderSrc + "\n" + networksTmplWideRowSrc
+	networksTmplWide         = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplWideSrc))
+	networksTmplWideNoHeader = template.Must(template.New("networks").Funcs(funcs).Parse(networksTmplWideRowSrc))
+)
 
 func Networks(outputFormat string, w *tabwriter.Writer, networks []*types.Network, header bool) error {
 	switch outputFormat {
