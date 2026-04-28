c9b084ff9c21137b0a80fb3603e8fbb8f0475e5d
Mikhail Swift <mikhail@replicated.com>
2026-04-28 13:37:04 -0400

    test: add policy pact tests

--- cli/print/policies.go
+++ cli/print/policies.go
@@ -17,29 +17,41 @@ var policiesTmplSrc = `ID	NAME	DESCRIPTION	READ ONLY
 var policiesTmpl = template.Must(template.New("policies").Parse(policiesTmplSrc))
 
 func Policies(outputFormat string, w *tabwriter.Writer, policies []*types.Policy) error {
-	if outputFormat == "table" {
+	switch outputFormat {
+	case "table":
 		if err := policiesTmpl.Execute(w, policies); err != nil {
 			return err
 		}
-	} else if outputFormat == "json" {
-		b, _ := json.MarshalIndent(policies, "", "  ")
+	case "json":
+		b, err := json.MarshalIndent(policies, "", "  ")
+		if err != nil {
+			return fmt.Errorf("marshal policies: %w", err)
+		}
 		if _, err := fmt.Fprintln(w, string(b)); err != nil {
 			return err
 		}
+	default:
+		return fmt.Errorf("invalid output format: %s", outputFormat)
 	}
 	return w.Flush()
 }
 
 func Policy(outputFormat string, w *tabwriter.Writer, policy *types.Policy) error {
-	if outputFormat == "table" {
+	switch outputFormat {
+	case "table":
 		if err := policiesTmpl.Execute(w, []*types.Policy{policy}); err != nil {
 			return err
 		}
-	} else if outputFormat == "json" {
-		b, _ := json.MarshalIndent(policy, "", "  ")
+	case "json":
+		b, err := json.MarshalIndent(policy, "", "  ")
+		if err != nil {
+			return fmt.Errorf("marshal policy: %w", err)
+		}
 		if _, err := fmt.Fprintln(w, string(b)); err != nil {
 			return err
 		}
+	default:
+		return fmt.Errorf("invalid output format: %s", outputFormat)
 	}
 	return w.Flush()
 }
