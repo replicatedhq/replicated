362aa05c2682d4400f245fd1194e69bd15754d93
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2026-05-08 09:00:41 -0700

    fix(cli): remove spurious 'release' from channel demote/un-demote examples

--- cli/cmd/channel_release_undemote.go
+++ cli/cmd/channel_release_undemote.go
@@ -13,11 +13,11 @@ func (r *runners) InitChannelReleaseUnDemote(parent *cobra.Command) {
 		Use:   "un-demote CHANNEL_ID_OR_NAME",
 		Short: "Un-demote a release from a channel",
 		Long:  "Un-demote a channel release from a channel using a channel sequence or release sequence.",
-		Example: `  # Un-demote a release from a channel by channel sequence
-  replicated channel release un-demote Beta --channel-sequence 15
+  	Example: `  # Un-demote a release from a channel by channel sequence
+  replicated channel un-demote Beta --channel-sequence 15
 
   # Un-demote a release from a channel by release sequence
-  replicated channel release un-demote Beta --release-sequence 12`,
+  replicated channel un-demote Beta --release-sequence 12`,
 		Args: cobra.ExactArgs(1),
 	}
 
