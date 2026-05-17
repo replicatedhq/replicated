e8bdd563e31a9dc51f444083f337500cc41bd92a
Marc Campbell <marc.e.campbell@gmail.com>
2026-05-17 09:54:51 -0600

    properly handle app type in api

--- client/app.go
+++ client/app.go
@@ -3,6 +3,9 @@ package client
 import (
 	"context"
 
+	"github.com/pkg/errors"
+	"github.com/replicatedhq/replicated/pkg/kotsclient"
+	"github.com/replicatedhq/replicated/pkg/platformclient"
 	"github.com/replicatedhq/replicated/pkg/types"
 )
 
@@ -49,13 +52,47 @@ func (c *Client) ListApps(excludeChannels bool) ([]types.AppAndChannels, error) 
 }
 
 func (c *Client) GetApp(appID string) (interface{}, error) {
-	return nil, nil
+	app, _, err := c.GetAppType(context.TODO(), appID, true)
+	if err != nil {
+		return nil, err
+	}
+	return app, nil
 }
 
 func (c *Client) CreateApp(appOptions interface{}) (interface{}, error) {
-	return nil, nil
+	switch opts := appOptions.(type) {
+	case string:
+		return c.KotsClient.CreateKOTSApp(context.TODO(), opts)
+	case kotsclient.CreateKOTSAppRequest:
+		return c.KotsClient.CreateKOTSApp(context.TODO(), opts.Name)
+	case *kotsclient.CreateKOTSAppRequest:
+		if opts == nil {
+			return nil, errors.New("create app options cannot be nil")
+		}
+		return c.KotsClient.CreateKOTSApp(context.TODO(), opts.Name)
+	case platformclient.AppOptions:
+		return c.PlatformClient.CreateApp(&opts)
+	case *platformclient.AppOptions:
+		if opts == nil {
+			return nil, errors.New("create app options cannot be nil")
+		}
+		return c.PlatformClient.CreateApp(opts)
+	default:
+		return nil, errors.Errorf("unsupported app options type %T", appOptions)
+	}
 }
 
 func (c *Client) DeleteApp(appID string) error {
-	return nil
+	app, appType, err := c.GetAppType(context.TODO(), appID, true)
+	if err != nil {
+		return err
+	}
+
+	if appType == "platform" {
+		return c.PlatformClient.DeleteApp(app.ID)
+	} else if appType == "kots" {
+		return c.KotsClient.DeleteKOTSApp(context.TODO(), app.ID)
+	}
+
+	return errors.Errorf("unknown app type %q", appType)
 }
