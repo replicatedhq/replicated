797e0b34b254a86afc3b0478d2d3a62f91869bd3
Marc Campbell <marc.e.campbell@gmail.com>
2026-07-27 13:44:58 -0600

    fix: make api commands execute v1 and v2 paths (#767)
    * fix: make api commands execute v1 and v2 paths
    
    Previously api get/post/put/patch only handled /v3 and silently exited 0
    for /v1 and /v2. Route all supported versions through the vendor HTTP
    client and error on unsupported paths.
    
    * style: gofmt api command files
    
    Align struct field spacing so CI gofmt check passes.

--- cli/cmd/api_patch.go
+++ cli/cmd/api_patch.go
@@ -1,23 +1,20 @@
 package cmd
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/replicatedhq/replicated/pkg/kotsclient"
 	"github.com/spf13/cobra"
 )
 
 func (r *runners) InitAPIPatch(parent *cobra.Command) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "patch",
+		Use:   "patch PATH",
 		Short: "Make ad-hoc PATCH API calls to the Replicated API",
 		Long: `This is essentially like curl for the Replicated API, but
 uses your local credentials and prints the response unmodified.
 
 We recommend piping the output to jq for easier reading.
 
-Pass the PATH of the request as the final argument. Do not include the host or version.`,
+Pass the PATH of the request as the final argument (including the API version
+prefix). Supported versions are /v1, /v2, and /v3. Do not include the host.`,
 		Example:      `replicated api patch /v3/customer/2VffY549paATVfHSGpJhjh6Ehpy -b '{"name":"Valuable Customer"}'`,
 		RunE:         r.apiPatch,
 		SilenceUsage: true,
@@ -31,32 +28,5 @@ Pass the PATH of the request as the final argument. Do not include the host or v
 }
 
 func (r *runners) apiPatch(cmd *cobra.Command, args []string) error {
-	path := args[0]
-
-	if !strings.HasPrefix(args[0], "/") {
-		path = fmt.Sprintf("/%s", args[0])
-	}
-	pathParts := strings.Split(path, "/")
-	// remove any empty parts
-	for i := len(pathParts) - 1; i >= 0; i-- {
-		if pathParts[i] == "" {
-			pathParts = append(pathParts[:i], pathParts[i+1:]...)
-		}
-	}
-
-	// v1 and v2 paths use platform client, v3 uses kots client
-	// split the path on the first slash to determine which client to use
-	if pathParts[0] == "v1" {
-
-	} else if pathParts[0] == "v3" {
-		kotsRestClient := kotsclient.VendorV3Client{HTTPClient: *r.platformAPI}
-		response, err := kotsRestClient.Patch(cmd.Context(), path, r.args.apiPatchBody)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("%s", response)
-	}
-
-	return nil
+	return r.doAPIRequest(cmd.Context(), "PATCH", args[0], r.args.apiPatchBody)
 }
